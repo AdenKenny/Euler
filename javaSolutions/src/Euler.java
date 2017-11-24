09ad3c2de09ca375361abdf56640443077547cca
Aden Kenny <adenkenny@gmail.com>
2017-11-24 15:30:03 +1300

    Problem 48 in python

--- javaSolutions/src/Euler.java
+++ javaSolutions/src/Euler.java
@@ -5,17 +5,17 @@ import java.util.Stack;
 import java.util.function.Function;
 
 public class Euler {
-	
-	private static long question10() {
-		
+
+	private static long problem10() {
+
 		int n = 2000000;
-		
+
 		boolean isPrime[] = new boolean[n + 1];
-		
-		for(int i = 2; i <= n; ++i) {
+
+		for (int i = 2; i <= n; ++i) {
 			isPrime[i] = true;
 		}
-		
+
 		for (int factor = 2; factor * factor <= n; ++factor) {
 			if (isPrime[factor]) {
 				for (int j = factor; factor * j <= n; ++j) {
@@ -23,77 +23,73 @@ public class Euler {
 				}
 			}
 		}
-		
+
 		long sum = 0;
-		for(int i = 2; i <= n; ++i) {
+		for (int i = 2; i <= n; ++i) {
 			if (isPrime[i]) {
 				sum += i;
 			}
 		}
-		
+
 		return sum;
-	}	
-	
-	public int problem26() {
-		return 0;
 	}
-	
+
 	public int problem40() {
-		
+
 		StringBuilder build = new StringBuilder();
 		build.append(0);
 
 		for (int i = 1; i < 1000000; ++i) {
 			build.append(i + "");
 		}
-		
+
 		String sum = build.toString();
-		
+
 		int total = 1;
-		
+
 		for (int i = 10; i < (1000000); i *= 10) {
 			int tmp = Integer.parseInt(sum.charAt(i) + "");
-						
+
 			total *= tmp;
 		}
-		
+
 		return total;
 	}
-	
+
 	public int problem44() {
-		
+
 		Function<Integer, Integer> pent = (val) -> {
-			return val * (3 * val - 1) / 2; 
+			return val * (3 * val - 1) / 2;
 		};
 
 		Set<Integer> pents = new HashSet<>();
-				
+
 		for (int i = 1; i < 10000; ++i) {
 			pents.add(pent.apply(i));
 		}
-				
+
 		Stack<Integer> stack = new Stack<>();
-		
+
 		for (int i : pents) {
 			for (int j : pents) {
 				int sum = i + j;
 				int difference = j - i;
-								
+
 				if (pents.contains(sum) && pents.contains(difference)) {
 					stack.push(Math.abs(i - j));
-				}				
+				}
 			}
 		}
-		
+
 		Collections.sort(stack);
-		
+
 		return stack.pop();
 	}
 
 	public Euler() {
-		System.out.println(question10());
+		System.out.println(problem40());
 	}
-		
+
 	public static void main(String[] args) {
 		new Euler();
 	}
